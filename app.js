9d243020671d31bd7d82fab2bc0202c31dd6c27c
Aung <akhantz250@gmail.com>
2022-03-28 23:25:41 +0800

    added functions to change brush color

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //variables
 const container = document.querySelector('.container')
-
 const clearBtn = document.querySelector('.remove');
+const blackBtn = document.querySelector('.black');
+const rainbowBtn = document.querySelector('.rainbow');
+let brushColor = 'black';
 
 //creates the drawing grid specified by the size
 function createDrawingArea(size){
@@ -9,15 +11,13 @@ function createDrawingArea(size){
         const newRow = document.createElement('div');
         newRow.classList.add('rows');
         container.appendChild(newRow);
-
         for(let j = 0; j < size; j++){
             const newSquare = document.createElement('div');
             newSquare.classList.add('squares');
             newRow.appendChild(newSquare);
-            newSquare.addEventListener('mouseover',(evt) => 
-                evt.target.style.backgroundColor = 'black');
+            newSquare.addEventListener('mouseover',(e) => 
+                brush(e));
         }
-
     }
 }
 
@@ -29,4 +29,42 @@ createDrawingArea(16);
 
 const allSquares = document.querySelectorAll('.squares');
 clearBtn.addEventListener("click",() => clearDrawing())
+
+
+//changes brush color
+
+function brush(e){
+    if(brushColor === 'black'){
+        e.target.style.backgroundColor = 'black';
+    }else if(brushColor === 'rainbow'){
+        e.target.style.backgroundColor = rainbow();
+    }
+}
+
+blackBtn.addEventListener('click',() => brushColor ='black');
+rainbowBtn.addEventListener('click',() => brushColor = 'rainbow');
+
+//generates a random color of the rainbow
+function rainbow(){
+    randInt = Math.floor((Math.random() * 7) + 1);
+    switch(randInt){
+        case 1:
+            return 'red';
+        case 2: 
+            return 'orange';
+        case 3:
+            return 'yellow';
+        case 4:
+            return 'green';
+        case 5:
+            return 'blue';
+        case 6:
+            return 'indigo';
+        case 7:
+            return 'violet';
+        
+    }
+}
+
+//add grid size options which removes all squares in the container and draws a new grid with a new size
 
