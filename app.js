3718d19037c10554de5e2a33aece0efc5a50aacd
Aung <akhantz250@gmail.com>
2022-05-15 00:41:59 +0800

    Fixed issue with selecting grid element when drawing and added coloured brush

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const rainbowBtn = document.querySelector('.rainbow');
 const grid16 = document.querySelector('.grid16');
 const grid32 = document.querySelector('.grid32');
 const grid64 = document.querySelector('.grid64');
+const colorBtn = document.querySelector('.color');
+const colorPicker = document.querySelector('.colorpicker');
 
 let brushColor = 'black';
+let currentColor = '#FF0000';
+let currentGrid = 16;
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 //creates the drawing grid specified by the size
 function createDrawingArea(size){
@@ -18,9 +26,9 @@ function createDrawingArea(size){
         for(let j = 0; j < size; j++){
             const newSquare = document.createElement('div');
             newSquare.classList.add('squares');
+            newSquare.addEventListener('mouseover',(e) => draw(e));
+            newSquare.addEventListener('mousedown',(e) => draw(e));
             newRow.appendChild(newSquare);
-            newSquare.addEventListener('mouseover',(e) => {if(e.buttons == 1)
-                brush(e)});
         }
     }
 }
@@ -33,20 +41,27 @@ function clearDrawing(){
 //starting grid 
 createDrawingArea(16);
 
-clearBtn.addEventListener("click",() => clearDrawing())
+//draw with brush color
 
-//changes brush color
-
-function brush(e){
+function draw(e){
+    if(e.type === 'mouseover' && !mouseDown) return;
     if(brushColor === 'black'){
         e.target.style.backgroundColor = 'black';
     }else if(brushColor === 'rainbow'){
-        e.target.style.backgroundColor = rainbow();
+        e.target.style.backgroundColor = rainbow();}
+    else if(brushColor === 'color'){
+        e.target.style.backgroundColor = currentColor;
     }
 }
 
-blackBtn.addEventListener('click',() => brushColor ='black');
-rainbowBtn.addEventListener('click',() => brushColor = 'rainbow');
+blackBtn.addEventListener('click',(e) => {brushColor ='black';
+colorSelected(e);});
+rainbowBtn.addEventListener('click',(e) => {brushColor = 'rainbow';
+colorSelected(e);});
+colorBtn.addEventListener('click', (e) => {brushColor = 'color';
+colorSelected(e);})
+colorPicker.onchange = (e) => currentColor = e.target.value;
+clearBtn.addEventListener("click",(e) => clearDrawing())
 
 //generates a random color of the rainbow
 function rainbow(){
@@ -72,13 +87,28 @@ function rainbow(){
 
 //add grid size options which removes all squares in the container and draws a new grid with a new size
 
-grid16.addEventListener('click',() => changeGridSize(16));
-grid32.addEventListener('click', () => changeGridSize(32));
-grid64.addEventListener('click', () => changeGridSize(64));
+grid16.addEventListener('click',(e) => changeGridSize(16,e));
+grid32.addEventListener('click', (e) => changeGridSize(32,e));
+grid64.addEventListener('click', (e) => changeGridSize(64,e));
 
-function changeGridSize(size){
+function changeGridSize(size, e){
+    gridSelected(e);
     const rows = document.querySelectorAll('.rows');
     rows.forEach((row) => container.removeChild(row));
     createDrawingArea(size);
-    
+}
+
+//selected btn
+
+function colorSelected(e){
+    let selectedBtn = document.querySelector('.brush.selected');
+    selectedBtn.classList.remove('selected');
+    e.target.classList.add('selected')
+}
+
+function gridSelected(e){
+    let selectedBtn = document.querySelector('.grid.selected');
+    selectedBtn.classList.remove('selected');
+    e.target.classList.add('selected');
+
 }
