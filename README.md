7d3a31fc9bcb5b268e9557410b9b9a99a199ae2b
Aung <akhantz250@gmail.com>
2022-06-28 23:50:22 +0800

    design

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# odin-etch-a-sketch
-Etch-a-sketch created using HTML, CSS and JavaScript. [Live Demo](https://akhantz250.github.io/odin-etch-a-sketch/)
+# Etch-a-sketch
+
+[Live Demo](https://akhantz250.github.io/odin-etch-a-sketch/)  
+Etch-a-sketch created using HTML, CSS and JavaScript.
+![This is a alt text.](https://raw.githubusercontent.com/akhantz250/odin-etch-a-sketch/main/screenshot.png 'Screenshot')
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //variables
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 const clearBtn = document.querySelector('.remove');
 const blackBtn = document.querySelector('.black');
 const rainbowBtn = document.querySelector('.rainbow');
@@ -10,7 +10,7 @@ const colorBtn = document.querySelector('.color');
 const colorPicker = document.querySelector('.colorpicker');
 
 let brushColor = 'black';
-let currentColor = '#FF0000';
+let currentColor = colorPicker.value;
 let currentGrid = 16;
 
 let mouseDown = false;
@@ -18,97 +18,102 @@ document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
 //creates the drawing grid specified by the size
-function createDrawingArea(size){
-    for(let i = 0; i < size; i++){
-        const newRow = document.createElement('div');
-        newRow.classList.add('rows');
-        container.appendChild(newRow);
-        for(let j = 0; j < size; j++){
-            const newSquare = document.createElement('div');
-            newSquare.classList.add('squares');
-            newSquare.addEventListener('mouseover',(e) => draw(e));
-            newSquare.addEventListener('mousedown',(e) => draw(e));
-            newRow.appendChild(newSquare);
-        }
+function createDrawingArea(size) {
+  for (let i = 0; i < size; i++) {
+    const newRow = document.createElement('div');
+    newRow.classList.add('rows');
+    container.appendChild(newRow);
+    for (let j = 0; j < size; j++) {
+      const newSquare = document.createElement('div');
+      newSquare.classList.add('squares');
+      newSquare.addEventListener('mouseover', (e) => draw(e));
+      newSquare.addEventListener('mousedown', (e) => draw(e));
+      newRow.appendChild(newSquare);
     }
+  }
 }
 
-function clearDrawing(){
-    const allSquares = document.querySelectorAll('.squares');
-    allSquares.forEach(sq => sq.style.backgroundColor = 'white');
+function clearDrawing() {
+  const allSquares = document.querySelectorAll('.squares');
+  allSquares.forEach((sq) => (sq.style.backgroundColor = 'white'));
 }
 
-//starting grid 
+//starting grid
 createDrawingArea(16);
 
 //draw with brush color
 
-function draw(e){
-    if(e.type === 'mouseover' && !mouseDown) return;
-    if(brushColor === 'black'){
-        e.target.style.backgroundColor = 'black';
-    }else if(brushColor === 'rainbow'){
-        e.target.style.backgroundColor = rainbow();}
-    else if(brushColor === 'color'){
-        e.target.style.backgroundColor = currentColor;
-    }
+function draw(e) {
+  if (e.type === 'mouseover' && !mouseDown) return;
+  if (brushColor === 'black') {
+    e.target.style.backgroundColor = 'black';
+  } else if (brushColor === 'rainbow') {
+    e.target.style.backgroundColor = rainbow();
+  } else if (brushColor === 'color') {
+    e.target.style.backgroundColor = currentColor;
+  }
 }
 
-blackBtn.addEventListener('click',(e) => {brushColor ='black';
-colorSelected(e);});
-rainbowBtn.addEventListener('click',(e) => {brushColor = 'rainbow';
-colorSelected(e);});
-colorBtn.addEventListener('click', (e) => {brushColor = 'color';
-colorSelected(e);})
-colorPicker.onchange = (e) => currentColor = e.target.value;
-clearBtn.addEventListener("click",(e) => clearDrawing())
+blackBtn.addEventListener('click', (e) => {
+  brushColor = 'black';
+  colorSelected(e);
+});
+rainbowBtn.addEventListener('click', (e) => {
+  brushColor = 'rainbow';
+  colorSelected(e);
+});
+colorBtn.addEventListener('click', (e) => {
+  brushColor = 'color';
+  colorSelected(e);
+});
+colorPicker.onchange = (e) => (currentColor = e.target.value);
+colorPicker.onclick = (e) => e.stopPropagation();
+clearBtn.addEventListener('click', (e) => clearDrawing());
 
 //generates a random color of the rainbow
-function rainbow(){
-    randInt = Math.floor((Math.random() * 7) + 1);
-    switch(randInt){
-        case 1:
-            return 'red';
-        case 2: 
-            return 'orange';
-        case 3:
-            return 'yellow';
-        case 4:
-            return 'green';
-        case 5:
-            return 'blue';
-        case 6:
-            return 'indigo';
-        case 7:
-            return 'violet';
-        
-    }
+function rainbow() {
+  randInt = Math.floor(Math.random() * 7 + 1);
+  switch (randInt) {
+    case 1:
+      return 'red';
+    case 2:
+      return 'orange';
+    case 3:
+      return 'yellow';
+    case 4:
+      return 'green';
+    case 5:
+      return 'blue';
+    case 6:
+      return 'indigo';
+    case 7:
+      return 'violet';
+  }
 }
 
 //add grid size options which removes all squares in the container and draws a new grid with a new size
 
-grid16.addEventListener('click',(e) => changeGridSize(16,e));
-grid32.addEventListener('click', (e) => changeGridSize(32,e));
-grid64.addEventListener('click', (e) => changeGridSize(64,e));
+grid16.addEventListener('click', (e) => changeGridSize(16, e));
+grid32.addEventListener('click', (e) => changeGridSize(32, e));
+grid64.addEventListener('click', (e) => changeGridSize(64, e));
 
-function changeGridSize(size, e){
-    gridSelected(e);
-    const rows = document.querySelectorAll('.rows');
-    rows.forEach((row) => container.removeChild(row));
-    createDrawingArea(size);
+function changeGridSize(size, e) {
+  gridSelected(e);
+  const rows = document.querySelectorAll('.rows');
+  rows.forEach((row) => container.removeChild(row));
+  createDrawingArea(size);
 }
 
 //selected btn
 
-function colorSelected(e){
-    let selectedBtn = document.querySelector('.brush.selected');
-    selectedBtn.classList.remove('selected');
-    e.target.classList.add('selected')
+function colorSelected(e) {
+  let selectedBtn = document.querySelector('.brush.selected');
+  selectedBtn.classList.remove('selected');
+  e.target.classList.add('selected');
 }
 
-function gridSelected(e){
-    let selectedBtn = document.querySelector('.grid.selected');
-    selectedBtn.classList.remove('selected');
-    e.target.classList.add('selected');
-
+function gridSelected(e) {
+  let selectedBtn = document.querySelector('.grid.selected');
+  selectedBtn.classList.remove('selected');
+  e.target.classList.add('selected');
 }
